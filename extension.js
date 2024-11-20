10aeca43c1b4db29ce0434c79f0c9b39aa2ecca6
hwschieding <hwschieding@gmail.com>
2024-11-19 20:50:22 -0500

    snippets, configurations and small compiler functionality

--- extension.js
+++ extension.js
@@ -4,18 +4,42 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+    const TERM_NAME = "NBC Compiler";
+    let nbcPath = vscode.Uri.file(vscode.workspace.getConfiguration().get("NXC.nbcCompilerPath"));
     console.log("Extension Running!");
-    const disposable = vscode.commands.registerCommand('nxchighlighter.helloworld', function () {
+    console.log(nbcPath);
+    context.subscriptions.push(vscode.commands.registerCommand('nxchighlighter.helloworld', function () {
         vscode.window.showInformationMessage('Hello World from nxchighlighter!');
-    });
-    const disposableTwo = vscode.commands.registerCommand('nxchighlighter.twoPlusTwo', function () {
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('nxchighlighter.twoPlusTwo', function () {
         vscode.window.showInformationMessage((2 + 2).toString());
-    });
-
-    context.subscriptions.push(disposable);
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('nxchighlighter.openTerminal', function () {
+        const terminal = getNBCTerminal(TERM_NAME);
+        terminal.sendText("echo 'Terminal Test Message'");
+        terminal.show();
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('nxchighlighter.testCompiler', function () {
+        const terminal = getNBCTerminal(TERM_NAME);
+        terminal.sendText(`'${nbcPath.fsPath}' -help`);
+    }))
 }
 
 function deactivate() {}
+
+function getNBCTerminal(terminalName) {
+    const terminals = vscode.window.terminals;
+    for(let i = 0; i < terminals.length; i++){
+        if(terminals[i].name == terminalName){
+            return terminals[i];
+        }
+    }
+    return createNBCTerminal(terminalName);
+}
+
+function createNBCTerminal(name){
+    return vscode.window.createTerminal(name);
+}
 
 module.exports = {
     activate,
