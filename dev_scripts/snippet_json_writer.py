b683ac93b795ff38e9537d4a3c8844205d158f6f
hwschieding <hwschieding@gmail.com>
2024-11-24 22:52:15 -0500

    various constant snippets

--- dev_scripts/snippet_json_writer.py
+++ dev_scripts/snippet_json_writer.py
@@ -4,11 +4,11 @@ ignored_keywords = ["unsigned"]
 def make_entry(f, name, description):
     print(f"{name}, {description}")
     snippet = {
-        "prefix":f"{name}()",
-        "body":f"{name}($1)$0",
+        "prefix":f"{name}",
+        "body":f"{name}",
         "description":description
     }
-    snippet_json = f"\"{name}\":" + json.dumps(snippet, indent=4) + ",\n"
+    snippet_json = f"\"{name}\":" + json.dumps(snippet, indent=None) + ",\n"
     print(snippet_json)
     f.write(snippet_json)
 
